c63e1e5939b95b0a96b489aec6c691af31ec88cd
Hansheng Chen <chenhansheng97@126.com>
2025-12-17 14:01:27 +0800

    Simplify MRO handling

--- modules/model_base.py
+++ modules/model_base.py
@@ -1,9 +1,8 @@
-import math
 import torch
 import comfy
 from functools import partial
 from enum import Enum
-from comfy.model_base import BaseModel, convert_tensor, QwenImage as _QwenImage, Flux as _Flux, utils
+from comfy.model_base import BaseModel, convert_tensor, QwenImage as _QwenImage, Flux as _Flux
 from .piflow_policies import POLICY_CLASSES
 from . import architectures
 
@@ -124,219 +123,27 @@ class BasePiFlow(BaseModel):
         return self.policy_class(model_output, x, sigma)
 
 
-class GMQwenImage(BasePiFlow, _QwenImage):
+class GMQwenImage(_QwenImage, BasePiFlow):
 
     def __init__(self, model_config, device=None):
-        super().__init__(model_config, architectures.GMQwenImageTransformer2DModel, device=device)
+        BasePiFlow.__init__(self, model_config, architectures.GMQwenImageTransformer2DModel, device=device)
         self.memory_usage_factor_conds = ("ref_latents",)
 
-    def extra_conds(self, **kwargs):
-        out = super().extra_conds(**kwargs)
-        cross_attn = kwargs.get("cross_attn", None)
-        if cross_attn is not None:
-            out['c_crossattn'] = comfy.conds.CONDRegular(cross_attn)
-        ref_latents = kwargs.get("reference_latents", None)
-        if ref_latents is not None:
-            latents = []
-            for lat in ref_latents:
-                latents.append(self.process_latent_in(lat))
-            out['ref_latents'] = comfy.conds.CONDList(latents)
 
-            ref_latents_method = kwargs.get("reference_latents_method", None)
-            if ref_latents_method is not None:
-                out['ref_latents_method'] = comfy.conds.CONDConstant(ref_latents_method)
-        return out
-
-
-class QwenImage(BasePiFlow, _QwenImage):
+class QwenImage(_QwenImage, BasePiFlow):
 
     def __init__(self, model_config, device=None):
-        super().__init__(model_config, architectures.QwenImageTransformer2DModelMod, device=device)
+        BasePiFlow.__init__(self, model_config, architectures.QwenImageTransformer2DModelMod, device=device)
         self.memory_usage_factor_conds = ("ref_latents",)
 
-    def extra_conds(self, **kwargs):
-        out = super().extra_conds(**kwargs)
-        cross_attn = kwargs.get("cross_attn", None)
-        if cross_attn is not None:
-            out['c_crossattn'] = comfy.conds.CONDRegular(cross_attn)
-        ref_latents = kwargs.get("reference_latents", None)
-        if ref_latents is not None:
-            latents = []
-            for lat in ref_latents:
-                latents.append(self.process_latent_in(lat))
-            out['ref_latents'] = comfy.conds.CONDList(latents)
 
-            ref_latents_method = kwargs.get("reference_latents_method", None)
-            if ref_latents_method is not None:
-                out['ref_latents_method'] = comfy.conds.CONDConstant(ref_latents_method)
-        return out
-
-
-class GMFlux(BasePiFlow, _Flux):
+class GMFlux(_Flux, BasePiFlow):
     def __init__(self, model_config, device=None):
-        super().__init__(model_config, architectures.GMFlux, device=device)
+        BasePiFlow.__init__(self, model_config, architectures.GMFlux, device=device)
         self.memory_usage_factor_conds = ("ref_latents",)
 
-    def concat_cond(self, **kwargs):
-        try:
-            # Handle Flux control loras dynamically changing the img_in weight.
-            num_channels = self.diffusion_model.img_in.weight.shape[1] // (self.diffusion_model.patch_size * self.diffusion_model.patch_size)
-        except:
-            # Some cases like tensorrt might not have the weights accessible
-            num_channels = self.model_config.unet_config["in_channels"]
 
-        out_channels = self.model_config.unet_config["out_channels"]
-
-        if num_channels <= out_channels:
-            return None
-
-        image = kwargs.get("concat_latent_image", None)
-        noise = kwargs.get("noise", None)
-        device = kwargs["device"]
-
-        if image is None:
-            image = torch.zeros_like(noise)
-
-        image = utils.common_upscale(image.to(device), noise.shape[-1], noise.shape[-2], "bilinear", "center")
-        image = utils.resize_to_batch_size(image, noise.shape[0])
-        image = self.process_latent_in(image)
-        if num_channels <= out_channels * 2:
-            return image
-
-        # inpaint model
-        mask = kwargs.get("concat_mask", kwargs.get("denoise_mask", None))
-        if mask is None:
-            mask = torch.ones_like(noise)[:, :1]
-
-        mask = torch.mean(mask, dim=1, keepdim=True)
-        mask = utils.common_upscale(mask.to(device), noise.shape[-1] * 8, noise.shape[-2] * 8, "bilinear", "center")
-        mask = mask.view(mask.shape[0], mask.shape[2] // 8, 8, mask.shape[3] // 8, 8).permute(0, 2, 4, 1, 3).reshape(mask.shape[0], -1, mask.shape[2] // 8, mask.shape[3] // 8)
-        mask = utils.resize_to_batch_size(mask, noise.shape[0])
-        return torch.cat((image, mask), dim=1)
-
-    def encode_adm(self, **kwargs):
-        return kwargs["pooled_output"]
-
-    def extra_conds(self, **kwargs):
-        out = super().extra_conds(**kwargs)
-        cross_attn = kwargs.get("cross_attn", None)
-        if cross_attn is not None:
-            out['c_crossattn'] = comfy.conds.CONDRegular(cross_attn)
-        # upscale the attention mask, since now we
-        attention_mask = kwargs.get("attention_mask", None)
-        if attention_mask is not None:
-            shape = kwargs["noise"].shape
-            mask_ref_size = kwargs["attention_mask_img_shape"]
-            # the model will pad to the patch size, and then divide
-            # essentially dividing and rounding up
-            (h_tok, w_tok) = (math.ceil(shape[2] / self.diffusion_model.patch_size), math.ceil(shape[3] / self.diffusion_model.patch_size))
-            attention_mask = utils.upscale_dit_mask(attention_mask, mask_ref_size, (h_tok, w_tok))
-            out['attention_mask'] = comfy.conds.CONDRegular(attention_mask)
-
-        guidance = kwargs.get("guidance", 3.5)
-        if guidance is not None:
-            out['guidance'] = comfy.conds.CONDRegular(torch.FloatTensor([guidance]))
-
-        ref_latents = kwargs.get("reference_latents", None)
-        if ref_latents is not None:
-            latents = []
-            for lat in ref_latents:
-                latents.append(self.process_latent_in(lat))
-            out['ref_latents'] = comfy.conds.CONDList(latents)
-
-            ref_latents_method = kwargs.get("reference_latents_method", None)
-            if ref_latents_method is not None:
-                out['ref_latents_method'] = comfy.conds.CONDConstant(ref_latents_method)
-        return out
-
-    def extra_conds_shapes(self, **kwargs):
-        out = {}
-        ref_latents = kwargs.get("reference_latents", None)
-        if ref_latents is not None:
-            out['ref_latents'] = list([1, 16, sum(map(lambda a: math.prod(a.size()), ref_latents)) // 16])
-        return out
-
-
-class Flux(BasePiFlow, _Flux):
+class Flux(_Flux, BasePiFlow):
     def __init__(self, model_config, device=None):
-        super().__init__(model_config, architectures.FluxMod, device=device)
+        BasePiFlow.__init__(self, model_config, architectures.FluxMod, device=device)
         self.memory_usage_factor_conds = ("ref_latents",)
-
-    def concat_cond(self, **kwargs):
-        try:
-            # Handle Flux control loras dynamically changing the img_in weight.
-            num_channels = self.diffusion_model.img_in.weight.shape[1] // (self.diffusion_model.patch_size * self.diffusion_model.patch_size)
-        except:
-            # Some cases like tensorrt might not have the weights accessible
-            num_channels = self.model_config.unet_config["in_channels"]
-
-        out_channels = self.model_config.unet_config["out_channels"]
-
-        if num_channels <= out_channels:
-            return None
-
-        image = kwargs.get("concat_latent_image", None)
-        noise = kwargs.get("noise", None)
-        device = kwargs["device"]
-
-        if image is None:
-            image = torch.zeros_like(noise)
-
-        image = utils.common_upscale(image.to(device), noise.shape[-1], noise.shape[-2], "bilinear", "center")
-        image = utils.resize_to_batch_size(image, noise.shape[0])
-        image = self.process_latent_in(image)
-        if num_channels <= out_channels * 2:
-            return image
-
-        # inpaint model
-        mask = kwargs.get("concat_mask", kwargs.get("denoise_mask", None))
-        if mask is None:
-            mask = torch.ones_like(noise)[:, :1]
-
-        mask = torch.mean(mask, dim=1, keepdim=True)
-        mask = utils.common_upscale(mask.to(device), noise.shape[-1] * 8, noise.shape[-2] * 8, "bilinear", "center")
-        mask = mask.view(mask.shape[0], mask.shape[2] // 8, 8, mask.shape[3] // 8, 8).permute(0, 2, 4, 1, 3).reshape(mask.shape[0], -1, mask.shape[2] // 8, mask.shape[3] // 8)
-        mask = utils.resize_to_batch_size(mask, noise.shape[0])
-        return torch.cat((image, mask), dim=1)
-
-    def encode_adm(self, **kwargs):
-        return kwargs["pooled_output"]
-
-    def extra_conds(self, **kwargs):
-        out = super().extra_conds(**kwargs)
-        cross_attn = kwargs.get("cross_attn", None)
-        if cross_attn is not None:
-            out['c_crossattn'] = comfy.conds.CONDRegular(cross_attn)
-        # upscale the attention mask, since now we
-        attention_mask = kwargs.get("attention_mask", None)
-        if attention_mask is not None:
-            shape = kwargs["noise"].shape
-            mask_ref_size = kwargs["attention_mask_img_shape"]
-            # the model will pad to the patch size, and then divide
-            # essentially dividing and rounding up
-            (h_tok, w_tok) = (math.ceil(shape[2] / self.diffusion_model.patch_size), math.ceil(shape[3] / self.diffusion_model.patch_size))
-            attention_mask = utils.upscale_dit_mask(attention_mask, mask_ref_size, (h_tok, w_tok))
-            out['attention_mask'] = comfy.conds.CONDRegular(attention_mask)
-
-        guidance = kwargs.get("guidance", 3.5)
-        if guidance is not None:
-            out['guidance'] = comfy.conds.CONDRegular(torch.FloatTensor([guidance]))
-
-        ref_latents = kwargs.get("reference_latents", None)
-        if ref_latents is not None:
-            latents = []
-            for lat in ref_latents:
-                latents.append(self.process_latent_in(lat))
-            out['ref_latents'] = comfy.conds.CONDList(latents)
-
-            ref_latents_method = kwargs.get("reference_latents_method", None)
-            if ref_latents_method is not None:
-                out['ref_latents_method'] = comfy.conds.CONDConstant(ref_latents_method)
-        return out
-
-    def extra_conds_shapes(self, **kwargs):
-        out = {}
-        ref_latents = kwargs.get("reference_latents", None)
-        if ref_latents is not None:
-            out['ref_latents'] = list([1, 16, sum(map(lambda a: math.prod(a.size()), ref_latents)) // 16])
-        return out
